2b5ef17b74b44b2ec899956ceb79dc84590bedc6
Nicole Cea <nivacea@gmail.com>
2022-04-25 23:18:15 -0400

    cart table

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,18 +5,27 @@ import { Link } from "react-router-dom";
 import { styled } from "@mui/material/styles";
 import { Button } from "@mui/material";
 import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
+import DeleteIcon from '@mui/icons-material/Delete';
+
+//Imports tabla MUI
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+
 import "../App.css";
 
 export default function Cart() {
+
   const { cart, removeFromCart, clear } = useContext(CartContext);
 
   const H1 = styled("h1")``;
 
   return (
     <>
-      <H1 sx={{ mx: 5, }} >
-        Carro
-      </H1>
+      <H1 sx={{ mx: 5 }}>Carro</H1>
 
       {cart.length == 0 ? (
         <Box
@@ -37,51 +46,52 @@ export default function Cart() {
             my: 5,
           }}
         >
-          <Box
-            className="itemHeader"
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <p>Imagen</p>
-            <p>Producto</p>
-            <p>Cantidad</p>
-            <p>Precio</p>
-            <p>Total</p>
-          </Box>
+          <TableContainer>
+            <Table>
+              <TableHead>
+                  <TableCell></TableCell>
+                  <TableCell>Producto</TableCell>
+                  <TableCell>Cantidad</TableCell>
+                  <TableCell>Precio</TableCell>
+                  <TableCell>Total</TableCell>
+                  <TableCell>Eliminar</TableCell>
+              </TableHead>
+              <TableBody>
+                {cart.map((item) => (
+                  <TableRow className="itemCart" key={item.id}>
+                    <TableCell>
+                      <img src={item.imagen} />
+                    </TableCell>
+                    <TableCell>
+                      <h3>{item.nombre}</h3>
+                    </TableCell>
+                    <TableCell>
+                      <p>{item.count}</p>
+                    </TableCell>
+                    <TableCell>
+                      <p>${item.precio}</p>
+                    </TableCell>
+                    <TableCell>
+                      <p>${item.precio * item.count}</p>
+                    </TableCell>
+                    <TableCell>
+                    <RemoveCircleOutlineIcon
+                      onClick={() => {
+                        removeFromCart(item.id);
+                      }}
+                    />
+                    </TableCell>
+                    
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
 
-          {cart.map((item) => (
-            <Box
-              className="itemCart"
-              key={item.id}
-              sx={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-            >
-              <img src={item.imagen} />
-              <h2>{item.nombre}</h2>
-              <p>{item.count}</p>
-              <p>{item.precio}</p>
-              <p>{item.precio * item.count}</p>
-              <RemoveCircleOutlineIcon
-                onClick={() => {
-                  removeFromCart(item.id);
-                }}
-              />
-            </Box>
-          ))}
-
-          <Button
+          <Button endIcon={<DeleteIcon />}
             onClick={() => {
               clear();
-            }}
-          >
+            }}>
             Vaciar carrito
           </Button>
         </Box>
--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,41 +1,47 @@
-import React, {useContext} from 'react';
-import { CartContext } from '../context/CartContext';
-import {ThemeProvider, createTheme, Box } from '@mui/system';
-import '../index.css';
-import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import React, { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+import { ThemeProvider, createTheme, Box } from "@mui/system";
+import "../index.css";
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
 import { Link } from "react-router-dom";
 
-
 export default function CartWidget() {
+  const { cart } = useContext(CartContext);
 
-    const { cart } = useContext(CartContext)
+  const count = cart.map((item) => item.count);
 
-    const theme = createTheme({
-        palette: {
-            background: {
-                main: '#000',
-                light: '#fff',
-                accent: '#0fd',
-            }
-        },
-    });
+  const totalCount = count.reduce((ac, item) => ac + item, 0);
 
-    return (
-        <ThemeProvider theme={theme}>
-            <Link to={`/cart`}>
-                <Box sx={{
-                    display: 'flex',
-                    flexDirection: 'row'
-                }}>
-                <h3>{cart.length}</h3>
-                <ShoppingBagIcon sx={{
-                    color: 'background.light',
-                    fontSize: 32,
-                    position: 'relative',
-                    top: 15
-                }} />
-                </Box>
-            </Link>
-        </ThemeProvider>
-    );
+  const theme = createTheme({
+    palette: {
+      background: {
+        main: "#000",
+        light: "#fff",
+        accent: "#0fd",
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Link to={`/cart`}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
+          <h3>{totalCount}</h3>
+          <ShoppingBagIcon
+            sx={{
+              color: "background.light",
+              fontSize: 32,
+              position: "relative",
+              top: 15,
+            }}
+          />
+        </Box>
+      </Link>
+    </ThemeProvider>
+  );
 }
